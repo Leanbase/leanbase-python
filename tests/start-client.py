dd3c7365df863d6f838b5743703841e34ef2d257
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2019-11-08 14:03:25 -0500

    Use current ports in start-client test

--- tests/start-client.py
+++ tests/start-client.py
@@ -9,7 +9,7 @@ logging.basicConfig(level=logging.DEBUG,
 leanbase.configure(
     api_key="7dc96185-bc8e-4c30-9a22-7d672b884452",
     team_id="5qgZJg1",
-    convey_host="http://localhost:9001/",
+    convey_host="http://localhost:9000/",
 )
 
 leanbase.await_initialisation(1.0)
