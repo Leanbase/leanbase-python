a168f44cb13763117cda6e9832e22d453ec113e3
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2019-11-17 08:58:27 -0500

    Add another test for overlap in partial rollouts

--- tests/test_evaluate.py
+++ tests/test_evaluate.py
@@ -31,6 +31,9 @@ f_store.set_feature(ga_feature.id, ga_feature)
 partial_feature = FeatureDefinition('GFGFNBG', FeatureGlobalStatus.PARTIAL, rollout_percentage=10)
 f_store.set_feature(partial_feature.id, partial_feature)
 
+partial_feature_2 = FeatureDefinition('NOICES', FeatureGlobalStatus.PARTIAL, rollout_percentage=10)
+f_store.set_feature(partial_feature_2.id, partial_feature_2)
+
 regular_user_attr = {'email': 'email@gmail.com', 'incarceration_count': 20}
 staff_user_attr = {'email': 'email@leanbase.io'}
 
@@ -88,3 +91,27 @@ class EvaluateTestCase(unittest.TestCase):
         logger.info(" %d %% was the rollout_percentage." %(partial_feature.rollout_percentage))
 
         self.assertAlmostEqual(truthy / (truthy + falsy) * 100, partial_feature.rollout_percentage, delta=0.5)
+
+    def test_evaluate_partial_overlap(self):
+        """ Two separate features with 10% rollout. Count users who have access
+        to both features. Should be 0.1 x 0.1 ~= 0.01 of total population. """
+        from tests import fixtures
+        truthy_both = 0
+        SAMPLE_SIZE = int(10e4)
+        for email in fixtures.random_emails(SAMPLE_SIZE):
+            regular_user_attr.update({ 'email': email })
+            result_1 = evaluate(
+                regular_user_attr,
+                partial_feature
+            )
+
+            result_2 = evaluate(
+                regular_user_attr,
+                partial_feature_2
+            )
+
+            if result_1 and result_2:
+                truthy_both += 1
+    
+        logger.info(' %d users among %d population saw both features.', truthy_both, SAMPLE_SIZE)
+        self.assertAlmostEqual(truthy_both / SAMPLE_SIZE * 100, partial_feature.rollout_percentage * partial_feature_2.rollout_percentage / 100, delta=0.5)
